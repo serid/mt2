2c3df23bef6b9b519a44322b77389ab0dc429097
Sergey <serridd@gmail.com>
2020-07-30 01:06:21 +0300

    Fibonacci numbers program.

--- mt2std.asm
+++ mt2std.asm
@@ -5,3 +5,24 @@ ret
 funcsub:
 sub esi, edi
 ret
+
+funcnot:
+mov eax, 0
+test esi, esi
+sete al
+mov esi, eax
+ret
+
+funceq: ; esi = (esi == edi)
+mov eax, 0
+cmp esi, edi
+sete al
+mov esi, eax
+ret
+
+funcneq: ; esi = (esi != edi)
+mov eax, 0
+cmp esi, edi
+setne al
+mov esi, eax
+ret
